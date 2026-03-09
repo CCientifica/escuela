0b7f77e9d6c4812bb28bbdfccc00a319a49caa18
Andrea Jaramillo <coordcientifico@clinicasagradocorazon.com.co>
2026-03-08 20:50:44 -0500

    Delete demo directory

--- demo/src/main/java/com/example/demo/repository/EscenarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Escenario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EscenarioRepository extends JpaRepository<Escenario, Long> {
-}
